1e651610aecb987395932c39791d2d85fb2d99fb
Joshua Hodge <joshuahodge@hotmail.com>
2018-02-28 19:59:20 +0000

    adding skeleton code for dsp module

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -96,8 +96,11 @@ void DspModuleSkeletonAudioProcessor::changeProgramName (int index, const String
 //==============================================================================
 void DspModuleSkeletonAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    dsp::ProcessSpec spec;
+    
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumOutputChannels();
 }
 
 void DspModuleSkeletonAudioProcessor::releaseResources()
@@ -130,33 +133,35 @@ bool DspModuleSkeletonAudioProcessor::isBusesLayoutSupported (const BusesLayout&
 }
 #endif
 
+void DspModuleSkeletonAudioProcessor::process (dsp::ProcessContextReplacing<float> context)
+{
+    //do your dsp processing and output here
+}
+
+void DspModuleSkeletonAudioProcessor::updateParameters()
+{
+    //be sure to update params for your dsp here
+}
+
+
 void DspModuleSkeletonAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
 {
     ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    //data structure that points to the buffer
+    dsp::AudioBlock<float> block (buffer);
+    
+    //update any parameters for your processes
+    updateParameters();
+    
+    //if you have more than one dsp process, put them in the process method
+    //JUCE dsp processes each have their own process method
+    process (dsp::ProcessContextReplacing<float> (block));
 }
 
 //==============================================================================
